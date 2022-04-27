7efbbcecd8061bf39d2b5760d9c9de3f2f751d30
molinarijess <lacerdajess@gmail.com>
2022-04-27 16:44:55 +0100

    added style and js

---
+++ script.js
@@ -0,0 +1,20 @@
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const secondHand = document.querySelector(".seconds-hand");
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const minuteHand = document.querySelector(".minutes-hand");
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + 90;
+  const hourHand = document.querySelector(".hour-hand");
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
